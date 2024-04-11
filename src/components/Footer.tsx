9d0fc6ea9014c00bace368f05b72d1509ee36f8c
Felipe-Belotto <felipeeduardo817@gmail.com>
2024-04-10 21:59:12 -0300

    adicionando espaçamento no footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='w-full bg-[#EEEEEE] h-[34px] flex justify-center items-center text-sm font-normal'>
+    <footer className='w-full bg-[#EEEEEE] h-[34px] p-2 flex justify-center items-center text-sm font-normal'>
       MKS sistemas © Todos os direitos reservados
     </footer>
   )
